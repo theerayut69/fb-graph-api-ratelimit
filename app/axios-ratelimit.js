6d9aaf0b8886920160e960ccbd9b9f19949ffbec
Theerayut Thanyaphoo <theerayut.t@mono.co.th>
2019-08-29 15:45:05 +0700

    add loop request

--- app/axios-ratelimit.js
+++ app/axios-ratelimit.js
@@ -5,8 +5,10 @@ let id = process.env.URL
 let access_token = process.env.FB_ACCESS_TOKEN
 let url = 'https://graph.facebook.com/v4.0/?id=' + id +'&access_token=' + access_token + '&fields=engagement'
 
-axios.get(url).then(res => {
-    console.log(res.data)
-}).catch(err => {
-    console.log(err)
-})
+for (var i = 0; i < 10; i++) {
+    axios.get(url).then(res => {
+        console.log(JSON.stringify(res.data))
+    }).catch(err => {
+        console.log(err)
+    })
+}
